5c9e6135efc94d2c412b697c5e0c53ba8d842d4d
Gomez <francisca.gomez-minet@capgemini.com>
2023-04-03 12:08:36 +0200

    metodos en el service

--- CapTicketUser/src/main/java/com/spring/service/UserServiceImpl.java
+++ CapTicketUser/src/main/java/com/spring/service/UserServiceImpl.java
@@ -1,5 +1,34 @@
 package com.spring.service;
 
-public class UserServiceImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.spring.model.User;
+import com.spring.repository.UserRepository;
+
+
+public class UserServiceImpl implements UserService {
+	
+	//private static final Logger log = LoggerFactory.getLogger(UserController.class);
+	
+	@Autowired
+	UserRepository userrepo;
+	
+	// -- LISTAR
+		public List<User> findAll(){
+			return userrepo.findAll();
+		}
+	
+	// -- MODDIFICAR
+		public void update(User user) {
+			userrepo.update(user);
+		}
+	
+	// -- ELIMINAR
+		public void deleteById(int id) {
+			userrepo.deleteById(id);
+		}
+	
 
 }
