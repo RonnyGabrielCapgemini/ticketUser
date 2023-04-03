0b785d233ca9e7a2f9d1b581c40e7e471b47eb37
Leira Garcia <julian.leira-garcia@capgemini.com>
2023-04-03 12:11:37 +0200

    Creacion de UserDTO

--- CapTicketUser/src/main/java/com/spring/response/UserDTO.java
+++ CapTicketUser/src/main/java/com/spring/response/UserDTO.java
@@ -1,5 +1,54 @@
 package com.spring.response;
 
-public class UserDTO {
+import java.io.Serializable;
 
+import com.spring.model.User;
+
+public class UserDTO implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	private int id;
+	private String name;
+	private String lastname;
+	private String mail;
+	
+	public static UserDTO of(User user) {
+		UserDTO userDto= new UserDTO();
+		userDto.setId(user.getId());
+		userDto.setName(user.getName());
+		userDto.setLastname(user.getLastName());
+		userDto.setMail(user.getMail());
+		return userDto;
+	}
+	
+	@Override
+	public String toString() {
+		return "UserDTO [id=" + id + ", name=" + name + ", lastname=" + lastname + ", mail=" + mail + "]";
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	
+	
 }
