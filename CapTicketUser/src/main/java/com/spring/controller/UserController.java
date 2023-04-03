2dc74dd42382237d8e0d531c8fa8d6db7a9810ad
Gomez <francisca.gomez-minet@capgemini.com>
2023-04-03 14:51:16 +0200

    controller principio

--- CapTicketUser/src/main/java/com/spring/controller/UserController.java
+++ CapTicketUser/src/main/java/com/spring/controller/UserController.java
@@ -1,5 +1,31 @@
 package com.spring.controller;
 
-public class UserController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+import com.spring.model.User;
+import com.spring.repository.UserRepository;
+import com.spring.response.UserDTO;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+	
+	@Autowired
+	private UserRepository urep;
+	
+	
+	@GetMapping()
+    public List<UserDTO> list() {
+        final List<User> all = urep.findAll();
+        
+        return UserDTO.of(all);
+    }
+	
 }
